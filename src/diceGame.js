451c5ab0c2a3aededcbd760280ca583c9fda804c
rokaya magdi <rokaiamagdi@gmail.com>
2019-09-19 06:39:41 +0200

    dice game exercise has been done git add .!

--- src/diceGame.js
+++ src/diceGame.js
@@ -1,14 +1,34 @@
 import React ,{Component} from 'react';
 import './dicegame.css' ;
-import Die from './dice'
+import Die from './die'
 class Diegame extends Component {
-render(){
-return(
-<div>
-<Die face="one"/>
-<Die face="two"/>
-</div>
-)}
-
+    static defaultProps={
+        faces : ["one","two","three","four","five","six"] 
+    };
+    constructor(props){
+            super(props);
+            this.state= { die1:"one", die2:"one" , rolling:false }
+    }
+    roll=(e)=>{
+        //choose random die face
+        const newdie1= this.props.faces[ Math.floor(Math.random()*this.props.faces.length) ]
+        const newdie2= this.props.faces[ Math.floor(Math.random()*this.props.faces.length) ]
+        //change the state of the die face
+        this.setState({ die1:newdie1 , die2:newdie2 , rolling: true   })
+        //wait one second then make rolling true
+        setTimeout( ()=>{
+            this.setState({rolling:false});
+        },1000  )
+    }
+    render(){
+        return(
+        <div>
+        <Die face={this.state.die1} Rolling={this.state.rolling}/>
+        <Die face={this.state.die2} Rolling={this.state.rolling}/>
+        <br/><button onClick={this.roll} disabled={this.state.rolling} > 
+             {this.state.rolling ?  "Rolling..."  : "Roll die" } 
+             </button>
+        </div>
+        )}
 }
 export default Diegame ;
--- src/die.js
+++ src/die.js
@@ -3,9 +3,6 @@ import './dicegame.css'
 
 class Die extends Component {
 render(){
-return ( 
-<div>
-<i className={ `fas fa-dice-${this.props.face}` }></i>
-</div>
-)}}
+return  <i className={ `die fas fa-dice-${this.props.face} ${this.props.Rolling? "shaking" : "" }` }></i>
+}}
 export default Die ;
